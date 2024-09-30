fe3fcfc04624e220a7e97c5fe43738fd7bc35a49
Joel Neander <jn223wj@student.lnu.se>
2024-09-30 14:35:15 +0200

    Man kan nu rotera och ändra ljusstyrka på bilder och jag har delat upp klasserna

--- src/ImageEditor.java
+++ src/ImageEditor.java
@@ -1,30 +1,28 @@
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
 public class ImageEditor {
+    private BufferedImage image;
 
     // Läser in bilden som skickas in.
-    public BufferedImage readImage(String filePath) throws IOException {
-        return ImageIO.read(new File(filePath));
+    public ImageEditor(String filePath) throws IOException {
+        this.image = ImageIO.read(new File(filePath));
     }
 
     // Sparar bilden till satt format och "path".
-    public void saveImage(BufferedImage image, String imageFormat, String outPutPath) throws IOException {
+    public void saveImage(String imageFormat, String outPutPath) throws IOException {
         ImageIO.write(image, imageFormat, new File(outPutPath));
     }
 
-    // Ändrar bildernas höjd och bredd.
-    public BufferedImage resizeImage(BufferedImage image, int width, int height) {
-        Image resizedImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        BufferedImage bufferedImage = new BufferedImage(width, height, image.getType());
-        Graphics2D g2d = bufferedImage.createGraphics();
-        g2d.drawImage(resizedImage, 0, 0, null);
-        g2d.dispose();
-        return bufferedImage;
+    // Returnerar den aktuella bilden
+    public BufferedImage getImage() {
+        return image;
     }
 
+    // Sätter en ny bild.
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
 }
--- src/L2.java
+++ src/L2.java
@@ -3,14 +3,24 @@ import java.io.IOException;
 
 public class L2 {
     public static void main(String[] args) {
-        ImageEditor editor = new ImageEditor();
         
         try {
+            ImageEditor editor = new ImageEditor("../image/robin.PNG");
 
-            BufferedImage image = editor.readImage("../image/robin.PNG");
+            ImageResizer resizer = new ImageResizer();
+            BufferedImage resizedImage = resizer.resizeImage(editor.getImage(), 600, 600);
+            editor.setImage(resizedImage);
+            editor.saveImage("png", "../image/robin1.PNG");
 
-            BufferedImage resizedImage = editor.resizeImage(image, 200, 200);
-            editor.saveImage(resizedImage, "png", "../image/robin1.PNG");
+            ImageRotator rotator = new ImageRotator();
+            BufferedImage rotatedImage = rotator.rotateImage(editor.getImage(), 45);
+            editor.setImage(rotatedImage);
+            editor.saveImage("png", "../image/robin2.PNG");
+
+            ImageBrightnessAdjuster brightnessAdjuster = new ImageBrightnessAdjuster();
+            BufferedImage brightenedImage = brightnessAdjuster.adjustBrightness(editor.getImage(), 1.5f);
+            editor.setImage(brightenedImage);
+            editor.saveImage("png", "../image/robin3.PNG");
             
         } catch (IOException e) {
             e.printStackTrace();
